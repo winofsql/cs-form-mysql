97561f6f9560bbeb4ccc54d09438f8cb1b910dbf
sworc <lightbox@nifty.com>
2021-10-19 15:09:29 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,7 @@ using System.Data;
 using System.Data.Odbc;
 using System.Windows.Forms;
 
-namespace cs_form_framework_mysql_datagridview
+namespace cs_form_mysql_datagridview
 {
     public partial class Form1 : Form
     {
