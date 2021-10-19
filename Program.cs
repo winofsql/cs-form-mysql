4060562a4322ed7f72be08ec5d286ee317743024
sworc <lightbox@nifty.com>
2021-10-19 15:10:00 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace cs_form_framework_mysql_datagridview
+namespace cs_form_mysql_datagridview
 {
     static class Program
     {
